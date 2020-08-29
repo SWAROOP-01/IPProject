1110d72b0110a48a0aa5ea1f4920492902a4795d
SWAROOP-01 <61293095+SWAROOP-01@users.noreply.github.com>
2020-08-29 21:26:46 +0530

    Valindating....

--- views/Authentucation/loginandregistration.php
+++ views/Authentucation/loginandregistration.php
@@ -1,4 +1,7 @@
 <?php
+	// Start Session
+	session_start();
+
 
 	// CONNECTING TO THE DATABASE
     $con = mysqli_connect("localhost" ,"root","","pawfectDB");
@@ -10,41 +13,64 @@
 	if(isset($_POST['reg_submit'])){
 		// DEFINING VARIBALES
 		$reg_username = ''; $reg_email = ''; $reg_mnumber = ''; $reg_pass1 = '';$reg_pass2 = '';$reg_date = ''; $reg_check = '';
+		$error_array = array();
 
 		$reg_username = strip_tags($_POST['reg_username']); 
         $reg_username = str_replace(' ','',$reg_username);
 		$reg_username = ucfirst(strtolower($reg_username));
+		$_SESSION['reg_username'] = $reg_username; // Storing varibale to a session
 
 		$reg_email = strip_tags($_POST['reg_email']); 
 		$reg_email = str_replace(' ','',$reg_email);
+		$_SESSION['reg_email'] = $reg_email; // Storing varibale to a session
 		
 		$reg_mnumber = strip_tags($_POST['reg_mnumber']); 
 		$reg_mnumber = str_replace(' ','',$reg_mnumber);
+		$_SESSION['reg_mnumber'] = $reg_mnumber; // Storing varibale to a session
 		
 		$reg_pass1 = strip_tags($_POST['reg_pass1']); 
+		$_SESSION['reg_pass1'] = $reg_pass1; // Storing varibale to a session
 
         $reg_pass2 = strip_tags($_POST['reg_pass2']); 
+		$_SESSION['reg_pass2'] = $reg_pass2; // Storing varibale to a session
 
-        $reg_date = date('Y-m-d');
+		$reg_date = date('Y-m-d');
+		
 
 		// ADDING constraints
-		if(strlen($reg_username) > 25 || strlen($reg_username) < 3 ){
-			echo "YOUR USERNAME MUST BE BETWEEN 2-25 Chars      ";
+		if(strlen($reg_username) > 15 || strlen($reg_username) < 3 ){
+			array_push($error_array,"Username must be between 3-13 characcters");
 		}
 
 		if($reg_pass1 != $reg_pass2){
-			echo "password DO NOT MATCH   ";
+			array_push($error_array,"Passwords do not match");
 		} else {
 			if(preg_match('/[^A-Za-z0-9]/', $reg_pass1)){
-				echo "Password Must contain Characters and numbers";
+				array_push($error_array,"You can only use characters and numbers");
 			}
 		}
 
 		if(strlen($reg_pass1) > 16 || strlen($reg_pass1) < 7){
-			echo "password must be between 8-15 Characters";
+			array_push($error_array,"password length must be between 7-16");
 		}
 
+		// ENCRYPT PASSWORD
+		if(empty($error_array)){
+			$reg_pass1 = md5($reg_pass1);
+			// echo $reg_pass1;
+		}
 		
+		//Check Duplicate Entries
+		$check_username_query = mysqli_query($con,"SELECT username FROM users WHERE username='$reg_username'");
+		$poll_1 = 0;
+
+		while(mysqli_num_rows($check_username_query) != 0 ){
+			$poll_1++;
+			$reg_username = $reg_username . "_" . $poll_1;
+			$check_username_query = mysqli_query($con,"SELECT username FROM users WHERE username='$reg_username'");
+			echo $reg_username;
+		}
+
 	}
 ?>
 
@@ -73,31 +99,60 @@
 
 			<form id="login" class="input-group" method="POST">
 				<input name='log_username' type="text" class="input-field" placeholder="Username" required>
-				<input name='log_password' type="text" class="input-field" placeholder="Enter Password" required>
+				<input name='log_password' type="password" class="input-field" placeholder="Enter Password" required>
 				<input name='log_check' type="checkbox" class="check-box"><span>Remember Password </span>
 				<button name='log_submit' class="submit-btn" type="submit">Log in</button>
 
 				<a style = 'color : black; font-size : .8rem; font-weight : 300;' href="#">Forgot Password?</a>
-				
-
- 
 			</form>
+
 			<form id="register" class="input-group" method="POST">
-				<input name='reg_username' type="text" class="input-field" placeholder="Username" required>
-				<input name='reg_email' type="email" class="input-field" placeholder="Email Id" required>
-				<input name='reg_mnumber' type="text" class="input-field" placeholder="Mobile Number" required>
-				<input name='reg_pass1' type="text" class="input-field" placeholder="Enter a Password" required>
-				<input name='reg_pass2' type="text" class="input-field" placeholder="Re-Enter Password" required>
+				<input name='reg_username' type="text" class="input-field" placeholder="Username" value="<?php
+						if(isset($_SESSION['reg_username'])){
+							echo $_SESSION['reg_username'];
+						}
+					?>" required>
+				<input name='reg_email' type="email" class="input-field" placeholder="Email Id" value="<?php
+						if(isset($_SESSION['reg_email'])){
+							echo $_SESSION['reg_email'];
+						}
+					?>" required>
+				<input name='reg_mnumber' type="text" class="input-field" placeholder="Mobile Number" value="<?php
+						if(isset($_SESSION['reg_mnumber'])){
+							echo $_SESSION['reg_mnumber'];
+						}
+					?>" required>
+				<input name='reg_pass1' type="password" class="input-field" placeholder="Enter a Password" required>
+				<input name='reg_pass2' type="password" class="input-field" placeholder="Re-Enter Password" required>
+
+				<!-- error message -->
+				<?php
+					if(isset($error_array)){
+				?>
+				<div style="font-size : .8rem; color: red;" class="input-field">
+					<?php
+					foreach($error_array as $val){
+							echo $val . " | ";
+						}
+					?>
+				</div>
+				<?php
+					}
+				?>
 
 				<input name='reg_check' type="checkbox" class="check-box"><span>I agree to the terms and conditions </span>
 				<button name='reg_submit' class="submit-btn" type="submit">Register</button>
+
+
+
 			</form>
+			
 		</div>
 		
 	</div>
 
 
-	<script type= 'text/javascript' src = './log_reg.js'></script>
+	<script type= 'text/javascript' src = '../../Public/Javascript/log_reg.js'></script>
 
 </body>
 </html>
